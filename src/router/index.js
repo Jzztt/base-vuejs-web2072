46d7b6f81d4e3cc9b87dbfb975f19b1118ba2770
Jettsyy <teazyyy2512@gmail.com>
2025-12-02 15:10:51 +0700

    feat : base router

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,15 @@
+import StudentList from '@/page/StudentList.vue'
 import { createRouter, createWebHistory } from 'vue-router'
-
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: StudentList,
+  },
+]
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes,
 })
 
 export default router
